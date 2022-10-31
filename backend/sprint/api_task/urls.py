eac2177dcdede62d32d4275b87d28666df4cc5d7
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 19:42:38 +0900

    feat: create api structure for task

--- backend/sprint/api_task/urls.py
+++ backend/sprint/api_task/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:project_id>/', views.task),
+    path('detail/<int:task_id>/', views.task_detail),
+    path('belong/<int:user_id>/', views.task_belong),
+    path('document/<int:task_id>/', views.task_document),
 ]
--- backend/sprint/api_task/views.py
+++ backend/sprint/api_task/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def task(request, project_id:int):
+    '''
+    [GET] Get task list of the project
+    [POST] Create new task for the project
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def task_detail(request, task_id:int):
+    '''
+    [GET] Get task detail
+    [PUT] Change task detail(ex. Assignee)
+    [DELETE] Delete the task
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def task_belong(request, user_id:int):
+    '''
+    [GET] Get task list of the user
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def task_document(request, task_id:int):
+    '''
+    [GET] Get document space list of the task
+    [POST] Connect documnet space and the task
+    [DELETE] Disconnect document space and the task
+    '''
+    # TODO
+    return HttpResponse(status=200)
