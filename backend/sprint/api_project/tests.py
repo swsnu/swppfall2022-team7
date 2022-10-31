e726650af382ba3ae81122edd59a1d09a5c6b933
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 20:38:33 +0900

    feat: create api task test

--- backend/sprint/api_project/tests.py
+++ backend/sprint/api_project/tests.py
@@ -3,19 +3,23 @@ from django.test import TestCase, Client
 # Create your tests here.
 class ProjectTestCase(TestCase):
     def setUp(self):
+        '''project test set up'''
         self.url = '/project/'
-    
+
     def test_project(self):
+        '''test api_project.project'''
         client = Client()
         response = client.get(self.url+'1/')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_project_detail(self):
+        '''test api_project.project_detail'''
         client = Client()
         response = client.get(self.url+'detail/1/')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_member(self):
+        '''test api_project.member'''
         client = Client()
         response = client.get(self.url+'member/1/')
         self.assertEqual(response.status_code, 200)
