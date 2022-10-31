9d402e1d0b478fd185d941e18860b56f741c553e
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 19:46:37 +0900

    feat: create api structure for document space

--- backend/sprint/api_document_space/urls.py
+++ backend/sprint/api_document_space/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:project_id>/', views.docuspace),
+    path('detail/<int:docuspace_id>/', views.docuspace_detail),
 ]
--- backend/sprint/api_document_space/views.py
+++ backend/sprint/api_document_space/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def docuspace(request, project_id:int):
+    '''
+    [GET] Get document space list of the project
+    [POST] Create new document space
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def docuspace_detail(request, docuspace_id:int):
+    '''
+    [GET] Get document space detail
+    [PUT] Change document space detail
+    [DELETE] Delete document space
+    '''
+    # TODO
+    return HttpResponse(status=200)
