1ea786e1429a442dbc34fa8740f2836b604380e9
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 19:29:24 +0900

    feat: create api structure for meeting

--- backend/sprint/api_meeting/urls.py
+++ backend/sprint/api_meeting/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:project_id>/', views.meeting),
+    path('detail/<int:meeting_id>/', views.meeting_detail),
 ]
--- backend/sprint/api_meeting/views.py
+++ backend/sprint/api_meeting/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def meeting(request, project_id:int):
+    '''
+    [GET] Get meeting list of the project
+    [POST] Create new meeting for the project
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def meeting_detail(request, meeting_id:int):
+    '''
+    [GET] Get meeting detail
+    [PUT] Change meeting detail
+    [DELETE] Delete the meeting
+    '''
+    # TODO
+    return HttpResponse(status=200)
