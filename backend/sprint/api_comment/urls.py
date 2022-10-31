0abbac5ae6e01d2489a0d2574bed654566a51130
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 19:55:35 +0900

    feat: create api structure for comment

--- backend/sprint/api_comment/urls.py
+++ backend/sprint/api_comment/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:task_id>/', views.comment),
+    path('detail/<int:comment_id>/', views.comment_detail),
 ]
--- backend/sprint/api_comment/views.py
+++ backend/sprint/api_comment/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def comment(request, task_id:int):
+    '''
+    [GET] Get comment list of the task
+    [POST] Create new comment for the task
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def comment_detail(request, comment_id:int):
+    '''
+    [GET] Get comment detail
+    [PUT] Change the comment
+    [DELETE] Delete the comment
+    '''
+    # TODO
+    return HttpResponse(status=200)
