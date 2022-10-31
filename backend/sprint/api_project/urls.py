f18950e0aa6d6f7a72b6fcc102195f3ab78e2727
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 19:20:48 +0900

    feat: create project api structure

--- backend/sprint/api_project/urls.py
+++ backend/sprint/api_project/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:user_id>/', views.project),
+    path('detail/<int:project_id>/', views.project_detail),
+    path('member/<int:project_id>/', views.member),
 ]
--- backend/sprint/api_project/views.py
+++ backend/sprint/api_project/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def project(request, user_id:int):
+    '''
+    [GET] Get project list of the user
+    [POST] Create new project for the user
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def project_detail(request, project_id:int):
+    '''
+    [GET] Get the detail of the project
+    [PUT] Change project detail
+    [DELETE] Delete the project
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def member(request, project_id:int):
+    '''
+    [GET] Get all members of the project
+    [PUT] Add or Subtract members
+    '''
+    # TODO
+    return HttpResponse(status=200)
