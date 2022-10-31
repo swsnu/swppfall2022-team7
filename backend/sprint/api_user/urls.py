866b43e7de3c1e5b546a3ce3d92fd802e095ac99
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 19:03:28 +0900

    feat: create api_user view structure

--- backend/sprint/api_user/urls.py
+++ backend/sprint/api_user/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test),
+    path('signup/', views.signup),
+    path('signin/', views.signin),
+    path('signout/', views.signout),
+    path('change/', views.change),
+    path('info/<user_id:int>', views.info),
 ]
--- backend/sprint/api_user/views.py
+++ backend/sprint/api_user/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def signup(request):
+    '''
+    [POST] User sign up
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def signin(request):
+    '''
+    [POST] User log in
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def signout(request):
+    '''
+    [GET] User log out
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def change(request):
+    '''
+    [PUT] Change User setting
+    [DELETE] Delete User
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def info(request):
+    '''
+    [GET] Get User info
+    '''
+    # TODO
+    return HttpResponse(status=200)
