b272a942f5210531a56a0e0471272b41b39bf1f5
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 21:08:09 +0900

    feat: create api document test

--- backend/sprint/api_document/tests.py
+++ backend/sprint/api_document/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class DocumentTestCase(TestCase):
+    def setUp(self):
+        self.url = '/documents/'
+
+    def test_document(self):
+        client = Client()
+        response = client.get(self.url+'1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_document_detail(self):
+        client = Client()
+        response = client.get(self.url+'detail/1/')
+        self.assertEqual(response.status_code, 200)
