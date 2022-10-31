f2c434c7a0195ab95ea04b9047c11712370266f2
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 19:51:12 +0900

    feat: create api structure for document

--- backend/sprint/api_document/urls.py
+++ backend/sprint/api_document/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:docuspace_id>/', views.document),
+    path('detail/<int:document_id>/', views.document_detail),
 ]
--- backend/sprint/api_document/views.py
+++ backend/sprint/api_document/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def document(request, docuspace_id:int):
+    '''
+    [GET] Get document list of the document space
+    [POST] Create document for the document space
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def document_detail(request, document_id:int):
+    '''
+    [GET] Get document detail
+    [PUT] Change detail of the document
+    [DELETE] Delete the document
+    '''
+    # TODO
+    return HttpResponse(status=200)
