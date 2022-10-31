b287d45017da57eea8fb45793810d21f49853e74
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 20:28:38 +0900

    feat: create api project test

--- backend/sprint/api_project/tests.py
+++ backend/sprint/api_project/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class ProjectTestCase(TestCase):
+    def setUp(self):
+        self.url = '/project/'
+    
+    def test_project(self):
+        client = Client()
+        response = client.get(self.url+'1/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_project_detail(self):
+        client = Client()
+        response = client.get(self.url+'detail/1/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_member(self):
+        client = Client()
+        response = client.get(self.url+'member/1/')
+        self.assertEqual(response.status_code, 200)
