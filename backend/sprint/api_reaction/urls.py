cd39d27fd7612617773ea0a7fd2fe888c6b44a53
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 19:59:08 +0900

    feat: create api structure for reaction

--- backend/sprint/api_reaction/urls.py
+++ backend/sprint/api_reaction/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:comment_id>/', views.reaction),
+    path('detail/<int:reaction_id>/', views.reaction_detail),
 ]
--- backend/sprint/api_reaction/views.py
+++ backend/sprint/api_reaction/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def reaction(request, comment_id:int):
+    '''
+    [GET] Get reaction list of the comment
+    [POST] Create new reaction for the comment
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+def reaction_detail(request, reaction_id:int):
+    '''
+    [GET] Get reaction detail
+    [PUT] Change the reaction
+    [DELETE] Delete the reaction
+    '''
+    # TODO
+    return HttpResponse(status=200)
