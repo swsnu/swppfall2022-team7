82714511caba2a7b9bd2f10b53b92d26eb333bd1
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 21:07:04 +0900

    feat: create api document space test

--- backend/sprint/api_document_space/tests.py
+++ backend/sprint/api_document_space/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class DocumentSpaceTestCase(TestCase):
+    def setUp(self):
+        self.url = '/document/'
+
+    def test_docuspace(self):
+        client = Client()
+        response = client.get(self.url+'1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_docuspace_detail(self):
+        client = Client()
+        response = client.get(self.url+'detail/1/')
+        self.assertEqual(response.status_code, 200)
