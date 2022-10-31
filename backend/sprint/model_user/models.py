86d5455a1655fe4adc439b45243eea008dfef221
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 11:32:11 +0900

    refactor: set backend code fit to pylint

--- backend/sprint/model_user/models.py
+++ backend/sprint/model_user/models.py
@@ -1,19 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Timetable(models.Model):
     schedule = models.JSONField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     content = models.TextField()
     checked = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
 class Image(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     image = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
