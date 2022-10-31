42444fe0b43130cb88a91a418e571b0d909d23e1
Sanghyun Yi <shyi0211@gmail.com>
2022-10-31 21:11:04 +0900

    feat: create api reaction test

--- backend/sprint/api_reaction/tests.py
+++ backend/sprint/api_reaction/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class ReactionTestCase(TestCase):
+    def setUp(self):
+        self.url = '/reaction/'
+
+    def test_reaction(self):
+        client = Client()
+        response = client.get(self.url+'1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_reaction_detail(self):
+        client = Client()
+        response = client.get(self.url+'detail/1/')
+        self.assertEqual(response.status_code, 200)
