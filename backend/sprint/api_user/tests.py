242665c8e6c431c38eb686b508af33dcc5a3bafb
Sanghyun Yi <shyi0211@gmail.com>
2022-11-01 11:40:12 +0900

    feat: add test for wrong method in api_user

--- backend/sprint/api_user/tests.py
+++ backend/sprint/api_user/tests.py
@@ -7,45 +7,90 @@ class UserTestCase(TestCase):
 
     def test_signup(self):
         client = Client()
-        response = client.get(self.url+'signup/')
+        url = self.url+'signup/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.post(url)
         self.assertEqual(response.status_code, 200)
 
     def test_signin(self):
         client = Client()
-        response = client.get(self.url+'signin/')
+        url = self.url+'signin/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.post(url)
         self.assertEqual(response.status_code, 200)
 
     def test_signout(self):
         client = Client()
-        response = client.get(self.url+'signout/')
+        url = self.url+'signout/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_change(self):
         client = Client()
-        response = client.get(self.url+'change/')
+        url = self.url+'change/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.put(url)
         self.assertEqual(response.status_code, 200)
 
     def test_info(self):
         client = Client()
-        response = client.get(self.url+'info/1/')
+        url = self.url+'info/1/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_timetable(self):
         client = Client()
-        response = client.get(self.url+'timetable/1/')
+        url = self.url+'timetable/1/'
+        # Wrong Method Test
+        response = client.delete(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_noti(self):
         client = Client()
-        response = client.get(self.url+'noti/1/')
+        url = self.url+'noti/1/'
+        # Wrong Method Test
+        response = client.put(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_noti_detail(self):
         client = Client()
-        response = client.get(self.url+'noti/detail/1/')
+        url = self.url+'noti/detail/1/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_image(self):
         client = Client()
-        response = client.get(self.url+'image/1/')
+        url = self.url+'image/1/'
+        # Wrong Method Test
+        # response = client.get(url)
+        # self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
         self.assertEqual(response.status_code, 200)
