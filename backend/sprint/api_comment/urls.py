b120141fbe5f457ac4f176213e065e885746999c
Sanghyun Yi <shyi0211@gmail.com>
2022-11-02 11:41:55 +0900

    fix: seperate safe and unsafe api for comment

--- backend/sprint/api_comment/urls.py
+++ backend/sprint/api_comment/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<int:task_id>/', views.comment),
+    path('<int:task_id>/m/', views.m_comment),
     path('detail/<int:comment_id>/', views.comment_detail),
+    path('detail/<int:comment_id>/m/', views.m_comment_detail),
 ]
--- backend/sprint/api_comment/views.py
+++ backend/sprint/api_comment/views.py
@@ -2,19 +2,33 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 
 # Create your views here.
-@require_http_methods(['GET', 'POST'])
+@require_http_methods(['GET'])
 def comment(request, task_id:int):
     '''
     [GET] Get comment list of the task
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+@require_http_methods(['POST'])
+def m_comment(request, task_id:int):
+    '''
     [POST] Create new comment for the task
     '''
     # TODO
     return HttpResponse(status=200)
 
-@require_http_methods(['GET', 'PUT', 'DELETE'])
+@require_http_methods(['GET'])
 def comment_detail(request, comment_id:int):
     '''
     [GET] Get comment detail
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+@require_http_methods(['PUT', 'DELETE'])
+def m_comment_detail(request, comment_id:int):
+    '''
     [PUT] Change the comment
     [DELETE] Delete the comment
     '''
