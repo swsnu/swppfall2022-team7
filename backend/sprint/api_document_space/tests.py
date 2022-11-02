c5a45cf0f6977c51236bd1da73d7b8284f518880
Sanghyun Yi <shyi0211@gmail.com>
2022-11-02 11:33:55 +0900

    fix: seperate safe and unsafe api for document space

--- backend/sprint/api_document_space/tests.py
+++ backend/sprint/api_document_space/tests.py
@@ -7,10 +7,40 @@ class DocumentSpaceTestCase(TestCase):
 
     def test_docuspace(self):
         client = Client()
-        response = client.get(self.url+'1/')
+        url = self.url+'1/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_m_docuspace(self):
+        client = Client()
+        url = self.url+'1/m/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.post(url)
         self.assertEqual(response.status_code, 200)
 
     def test_docuspace_detail(self):
         client = Client()
-        response = client.get(self.url+'detail/1/')
+        url = self.url+'detail/1/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_m_docuspace_detail(self):
+        client = Client()
+        url = self.url+'detail/1/m/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.put(url)
         self.assertEqual(response.status_code, 200)
--- backend/sprint/api_document_space/views.py
+++ backend/sprint/api_document_space/views.py
@@ -2,19 +2,33 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 
 # Create your views here.
-@require_http_methods(['GET', 'POST'])
+@require_http_methods(['GET'])
 def docuspace(request, project_id:int):
     '''
     [GET] Get document space list of the project
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+@require_http_methods(['POST'])
+def m_docuspace(request, project_id:int):
+    '''
     [POST] Create new document space
     '''
     # TODO
     return HttpResponse(status=200)
 
-@require_http_methods(['GET', 'PUT', 'DELETE'])
+@require_http_methods(['GET'])
 def docuspace_detail(request, docuspace_id:int):
     '''
     [GET] Get document space detail
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+@require_http_methods(['PUT', 'DELETE'])
+def m_docuspace_detail(request, docuspace_id:int):
+    '''
     [PUT] Change document space detail
     [DELETE] Delete document space
     '''
