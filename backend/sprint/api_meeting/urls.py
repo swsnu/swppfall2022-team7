fb2f85a39f34152191c882b1ddddedc688903c03
Sanghyun Yi <shyi0211@gmail.com>
2022-11-02 11:31:03 +0900

    fix: seperate safe and unsafe api for meeting

--- backend/sprint/api_meeting/urls.py
+++ backend/sprint/api_meeting/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<int:project_id>/', views.meeting),
+    path('<int:project_id>/m/', views.m_meeting),
     path('detail/<int:meeting_id>/', views.meeting_detail),
+    path('detail/<int:meeting_id>/m/', views.m_meeting_detail),
 ]
--- backend/sprint/api_meeting/views.py
+++ backend/sprint/api_meeting/views.py
@@ -2,19 +2,33 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 
 # Create your views here.
-@require_http_methods(['GET', 'POST'])
+@require_http_methods(['GET'])
 def meeting(request, project_id:int):
     '''
     [GET] Get meeting list of the project
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+@require_http_methods(['POST'])
+def m_meeting(request, project_id:int):
+    '''
     [POST] Create new meeting for the project
     '''
     # TODO
     return HttpResponse(status=200)
 
-@require_http_methods(['GET', 'PUT', 'DELETE'])
+@require_http_methods(['GET'])
 def meeting_detail(request, meeting_id:int):
     '''
     [GET] Get meeting detail
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+@require_http_methods(['PUT', 'DELETE'])
+def m_meeting_detail(request, meeting_id:int):
+    '''
     [PUT] Change meeting detail
     [DELETE] Delete the meeting
     '''
