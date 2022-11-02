bc5a06d3746d97cab6c054ff8b856cc0c3b5646e
Sanghyun Yi <shyi0211@gmail.com>
2022-11-02 11:19:47 +0900

    fix: seperate safe and unsafe api for reaction

--- backend/sprint/api_reaction/tests.py
+++ backend/sprint/api_reaction/tests.py
@@ -7,10 +7,40 @@ class ReactionTestCase(TestCase):
 
     def test_reaction(self):
         client = Client()
-        response = client.get(self.url+'1/')
+        url = self.url+'1/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_m_reaction(self):
+        client = Client()
+        url = self.url+'1/m/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.post(url)
         self.assertEqual(response.status_code, 200)
 
     def test_reaction_detail(self):
         client = Client()
-        response = client.get(self.url+'detail/1/')
+        url = self.url+'detail/1/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_m_reaction_detail(self):
+        client = Client()
+        url = self.url+'detail/1/m/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.put(url)
         self.assertEqual(response.status_code, 200)
--- backend/sprint/api_reaction/views.py
+++ backend/sprint/api_reaction/views.py
@@ -2,19 +2,33 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 
 # Create your views here.
-@require_http_methods(['GET', 'POST'])
+@require_http_methods(['GET'])
 def reaction(request, comment_id:int):
     '''
     [GET] Get reaction list of the comment
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+@require_http_methods(['POST'])
+def m_reaction(request, comment_id:int):
+    '''
     [POST] Create new reaction for the comment
     '''
     # TODO
     return HttpResponse(status=200)
 
-@require_http_methods(['GET', 'PUT', 'DELETE'])
+@require_http_methods(['GET'])
 def reaction_detail(request, reaction_id:int):
     '''
     [GET] Get reaction detail
+    '''
+    # TODO
+    return HttpResponse(status=200)
+
+@require_http_methods(['PUT', 'DELETE'])
+def m_reaction_detail(request, reaction_id:int):
+    '''
     [PUT] Change the reaction
     [DELETE] Delete the reaction
     '''
