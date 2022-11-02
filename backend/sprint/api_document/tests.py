4354ca58fd31f4c8ef594556beb826151a61387c
Sanghyun Yi <shyi0211@gmail.com>
2022-11-02 11:39:17 +0900

    fix: seperate safe and unsafe api for document

--- backend/sprint/api_document/tests.py
+++ backend/sprint/api_document/tests.py
@@ -7,10 +7,40 @@ class DocumentTestCase(TestCase):
 
     def test_document(self):
         client = Client()
-        response = client.get(self.url+'1/')
+        url = self.url+'1/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_m_document(self):
+        client = Client()
+        url = self.url+'1/m/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.post(url)
         self.assertEqual(response.status_code, 200)
 
     def test_document_detail(self):
         client = Client()
-        response = client.get(self.url+'detail/1/')
+        url = self.url+'detail/1/'
+        # Wrong Method Test
+        response = client.post(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_m_document_detail(self):
+        client = Client()
+        url = self.url+'detail/1/m/'
+        # Wrong Method Test
+        response = client.get(url)
+        self.assertEqual(response.status_code, 405)
+        # Right Test
+        response = client.put(url)
         self.assertEqual(response.status_code, 200)
--- backend/sprint/api_document/urls.py
+++ backend/sprint/api_document/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<int:docuspace_id>/', views.document),
+    path('<int:docuspace_id>/m/', views.m_document),
     path('detail/<int:document_id>/', views.document_detail),
+    path('detail/<int:document_id>/m/', views.m_document_detail),
 ]
