45ab4601ad15e92ecef2b5dbca9cdb0e06db9f2a
poding84 <a42072130@gmail.com>
2022-11-03 11:55:46 +0900

    feat: register all the models to admin page

--- backend/sprint/model_project/admin.py
+++ backend/sprint/model_project/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+class UserProjectAdmin(admin.ModelAdmin):
+    pass
+
+class MeetingAdmin(admin.ModelAdmin):
+    pass
+
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+class ReactionAdmin(admin.ModelAdmin):
+    pass
+
+class DocumentSpaceAdmin(admin.ModelAdmin):
+    pass
+
+class TaskDocumentSpaceAdmin(admin.ModelAdmin):
+    pass
+
+class DocumentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(UserProject, UserProjectAdmin)
+admin.site.register(Meeting, MeetingAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Reaction, ReactionAdmin)
+admin.site.register(DocumentSpace, DocumentSpaceAdmin)
+admin.site.register(TaskDocumentSpace, TaskDocumentSpaceAdmin)
+admin.site.register(Document, DocumentAdmin)
--- backend/sprint/model_user/admin.py
+++ backend/sprint/model_user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class TimetableAdmin(admin.ModelAdmin):
+    pass
+
+class NotificationAdmin(admin.ModelAdmin):
+    pass
+
+class ImageAdmin(admin.ModelAdmin) :
+    pass
+
+admin.site.register(Timetable, TimetableAdmin)
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(Image, ImageAdmin)
