d59692ea12e65b96308d90be9be6ad55bd4dc048
rosyrosie <kshunn@snu.ac.kr>
2022-11-03 22:05:35 +0900

    feat: add NewProject route and link

--- frontend/sprint/src/pages/Home.tsx
+++ frontend/sprint/src/pages/Home.tsx
@@ -1,13 +1,17 @@
 import MyTasks from '@components/MyTasks';
 import ProjectCard from '@components/ProjectCard';
 import { Button, Row } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div className='home'>
       <div className='project-title-tab'>
         Projects
-        <Button type='primary'>New Project</Button>
+        <Button type='primary' onClick={() => navigate('new-project')}>
+          New Project
+        </Button>
       </div>
       <Row gutter={[24, 24]}>
         <ProjectCard />
---
+++ frontend/sprint/src/pages/NewProject.tsx
@@ -0,0 +1,9 @@
+const NewProject: React.FC = () => {
+  return (
+    <div>
+      New Project
+    </div>
+  );
+};
+
+export default NewProject;
