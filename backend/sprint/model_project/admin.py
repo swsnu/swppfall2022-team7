61dbe23c60bc0bfb17d78cc78000945f92621388
poding84 <a42072130@gmail.com>
2022-11-03 16:34:11 +0900

    fix: remove unused import

--- backend/sprint/model_project/admin.py
+++ backend/sprint/model_project/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Project, UserProject, Meeting, Task, Comment, Reaction, Document, DocumentSpace, TaskDocumentSpace
 
 # Register your models here.
 
