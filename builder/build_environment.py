78d805d9469abda4ba1eae5372b30526f2f1b2e9
Sanghyun Yi <shyi0211@gmail.com>
2022-11-03 19:21:23 +0900

    fix: environment setting error

--- builder/build_environment.py
+++ builder/build_environment.py
@@ -1,5 +1,4 @@
 import os
 
-os.system('tar -cvf ../backend/env.tar ../backend/.env')
-os.system('travis encrypt-file -f --com ../backend/env.tar')
-os.system('mv env.tar.enc ../backend/env.tar.enc')
+os.system('travis encrypt-file -f --com ../backend/.env')
+os.system('mv .env.enc ../backend/env.enc')
