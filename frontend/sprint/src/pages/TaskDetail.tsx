4d7cac16f7f931d53e491464a4da4d6a7db2cc55
Alb3it <hjjoo389@gmail.com>
2022-11-05 19:39:11 +0900

    feat: add document feature in TaskDetail page

--- frontend/sprint/src/pages/TaskDetail.tsx
+++ frontend/sprint/src/pages/TaskDetail.tsx
@@ -1,22 +1,46 @@
-import { Input, Avatar, Comment, Tooltip, Button } from 'antd';
+import { Input, Avatar, Comment, Tooltip, Button, Collapse, Upload, message } from 'antd';
+import type { UploadProps } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
 import React, { useState, useEffect, createElement } from 'react';
-import { DislikeFilled, DislikeOutlined, LikeFilled, LikeOutlined } from '@ant-design/icons';
+import { DislikeFilled, DislikeOutlined, LikeFilled, LikeOutlined, InboxOutlined } from '@ant-design/icons';
 
 interface TaskDetailProps {
   taskName: string
   taskContent: string
 }
 
+const { Dragger } = Upload;
+
+const props: UploadProps = {
+  name: 'file',
+  multiple: true,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange: (info) => {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      void message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      void message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop: (e) => {
+    console.log('Dropped files', e.dataTransfer.files);
+  }
+};
+
 const TaskDetail: React.FC<TaskDetailProps> = ({ taskName, taskContent }: TaskDetailProps) => {
   const [edit, setEdit] = useState(false);
   const [editedName, setEditedName] = useState(taskName);
   const [taskInfo, setTaskInfo] = useState({ name: taskName, content: taskContent });
   const [editedContent, setEditedContent] = useState(taskContent);
-
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
   const [action, setAction] = useState<string | null>(null);
+
+  const { Panel } = Collapse;
 
   useEffect(() => {
     setTaskInfo({ name: taskName, content: taskContent });
@@ -138,6 +162,57 @@ const TaskDetail: React.FC<TaskDetailProps> = ({ taskName, taskContent }: TaskDe
               </Tooltip>
             }
           />
+        </div>
+        <div className="documents-container">
+          <div className="document-header">Projects Documents<Button className="document-confirm"size='small'>Confirm</Button></div>
+          <Collapse accordion>
+            <Panel header='Summary' key='1'>
+              <div className="document-container">
+                <div className="document-left">
+                  <div className="document-uploaded">
+                    <div className="uploaded-file">SummaryForm.hwp</div>
+                    <div className="file-info">
+                      <div className="uploaded-time">2022-11-04-11:05</div>
+                      <div className="file-uploader">SangHyun Yi</div>
+                    </div>
+                  </div>
+                  <div className="document-uploaded">
+                    <div className="uploaded-file">First Draft.hwp</div>
+                    <div className="file-info">
+                      <div className="uploaded-time">2022-11-05-15:13</div>
+                      <div className="file-uploader">SangHun Kim</div>
+                    </div>
+                  </div>
+                  <div className="document-uploaded">
+                    <div className="uploaded-file">SecondDraft.hwp</div>
+                    <div className="file-info">
+                      <div className="uploaded-time">2022-11-07-01:45</div>
+                      <div className="file-uploader">SeokWoo Choi</div>
+                    </div>
+                  </div>
+                  <div className="document-uploaded">
+                    <div className="uploaded-file">FinalDraft.hwp</div>
+                    <div className="file-info">
+                      <div className="uploaded-time">2022-11-09-22:34</div>
+                      <div className="file-uploader">HyungJin Joo</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="document-right">
+                  <Dragger {...props}>
+                    <p className="ant-upload-drag-icon">
+                      <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                    <p className="ant-upload-hint">
+                      Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                      band files
+                    </p>
+                  </Dragger>
+                </div>
+              </div>
+            </Panel>
+          </Collapse>
         </div>
       </div>
     </div>
