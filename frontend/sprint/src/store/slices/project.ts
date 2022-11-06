09003d3134f86f3cd567d0d89b5292f6dd514032
rosyrosie <kshunn@snu.ac.kr>
2022-11-06 21:58:51 +0900

    feat: link task information with redux

--- frontend/sprint/src/store/slices/project.ts
+++ frontend/sprint/src/store/slices/project.ts
@@ -16,6 +16,15 @@ export const projectSlice = createSlice({
     addTask: (state, action: PayloadAction<{ projectId: number, newTask: TaskType }>) => {
       const project = state.find(project => project.id === action.payload.projectId);
       if (project !== undefined) project.tasks.push(action.payload.newTask);
+    },
+    editTask: (state, action: PayloadAction<{ projectId: number, taskId: number, newTaskName: string, newTaskDescription: string }>) => {
+      const project = state.find(project => project.id === action.payload.projectId);
+      if (project !== undefined) {
+        const task = project.tasks.find(task => task.id === action.payload.taskId);
+        if (task === undefined) return;
+        task.name = action.payload.newTaskName;
+        task.description = action.payload.newTaskDescription;
+      }
     }
   }
 });
