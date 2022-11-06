84db4a7c8acd843992f49aa8b35d81f3e1a52e8f
rosyrosie <kshunn@snu.ac.kr>
2022-11-06 15:57:09 +0900

    test: add MemoryRouter to test render

--- frontend/sprint/src/pages/ProjectIntro.test.tsx
+++ frontend/sprint/src/pages/ProjectIntro.test.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import ProjectIntro from './ProjectIntro';
 
 describe('project intro test', () => {
   let AD: JSX.Element;
   beforeAll(() => {
-    AD = <ProjectIntro />;
+    AD = <MemoryRouter><ProjectIntro /></MemoryRouter>;
     global.matchMedia = global.matchMedia ?? function () {
       return {
         addListener: jest.fn(),
