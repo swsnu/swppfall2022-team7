f493557231d51857b88f85e5f3b43126deeda1e3
Sanghyun Yi <shyi0211@gmail.com>
2022-11-22 02:01:14 +0900

    chore: set up https nginx setting

--- backend/sprint/run.py
+++ backend/sprint/run.py
@@ -1,4 +1,5 @@
 import os
 
 os.system("python manage.py migrate")
+os.system("python manage.py collectstatic")
 os.system("gunicorn sprint.wsgi:application --bind 0.0.0.0:8000 --timeout=1000 --workers 3")
