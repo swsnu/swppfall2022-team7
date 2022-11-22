b3834b6359f5220b524f72922325f5195e3d64c7
Sanghyun Yi <shyi0211@gmail.com>
2022-11-22 20:10:26 +0900

    DRF & Swagger setting + Nginx https configuration (#92)
    * chore: set up swagger for api spec
    
    * chore: set up https nginx setting
    
    * chore: pylint match
    
    * fix: resolve admin csrf token issue
    
    * chore: fix some lines for pylint

---
+++ backend/sprint/api_user/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+class RequestSignupPOSTSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=20)
+    password = serializers.CharField(max_length=20)
+    email = serializers.CharField(max_length=20)
+
+class ResponseSignupPOSTSerializer200(serializers.Serializer):
+    id = serializers.IntegerField()
+    username = serializers.CharField()
+    email = serializers.CharField()
+
+class ResponseSignupPOSTSerializer401(serializers.Serializer):
+    pass
