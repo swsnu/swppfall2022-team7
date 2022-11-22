60325eadb312d3e02f5b6857a515aaabe5574d79
Alb3it <hjjoo389@gmail.com>
2022-11-22 21:38:19 +0900

    feat: implement dissolve project modal page

--- frontend/sprint/src/pages/ProjectManage.tsx
+++ frontend/sprint/src/pages/ProjectManage.tsx
@@ -2,12 +2,14 @@ import { selectProject } from '@store/slices/project';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useMemo, useState } from 'react';
-import { List, Avatar, Button, Modal, AutoComplete } from 'antd';
+import { List, Avatar, Button, Modal, AutoComplete, Input } from 'antd';
 import AutoOption from '@components/AutoOption';
 import { dummyMembers, MemberType } from '@utils/dummy';
 
 const ProjectManage: React.FC = () => {
   const [openAdd, setOpenAdd] = useState(false);
+  const [openDissolve, setOpenDissolve] = useState(false);
+  const [dissolve, setDissolve] = useState('');
   const [email, setEmail] = useState('');
   const [inviteList, setInviteList] = useState<MemberType[]>([]);
   const onInviteClick: () => void = () => {
@@ -21,7 +23,6 @@ const ProjectManage: React.FC = () => {
   const projectInfo = useMemo(() => (
     projectState.find(project => project.id === parseInt(projectId ?? '0'))
   ), [projectId]);
-  const project = projectState.find(project => project.id === parseInt(projectId ?? '0'));
 
   const onAddConfirmClicked = (): void => {
     setOpenAdd(false);
@@ -31,6 +32,16 @@ const ProjectManage: React.FC = () => {
   const onAddCancelClicked = (): void => {
     setOpenAdd(false);
     setInviteList([]);
+  };
+
+  const onDissolveConfirmClicked = (): void => {
+    setOpenDissolve(false);
+    setDissolve('');
+  };
+
+  const onDissolveCancelClicked = (): void => {
+    setOpenDissolve(false);
+    setDissolve('');
   };
 
   return (
@@ -45,7 +56,7 @@ const ProjectManage: React.FC = () => {
           </div>
           <List
             itemLayout="horizontal"
-            dataSource={project?.members}
+            dataSource={projectInfo?.members}
             renderItem={item => (
               <List.Item
                 actions={[<a key="list-delete">delete</a>]}
@@ -68,7 +79,7 @@ const ProjectManage: React.FC = () => {
               This will lead to irreversible consequence, like permanent loss of project files, tasks, etc. Please be certain.
             </div>
           </div>
-          <Button danger>Dissolve Project</Button>
+          <Button danger onClick={() => { setOpenDissolve(true); }}>Dissolve Project</Button>
         </div>
       </div>
       <Modal
@@ -127,6 +138,25 @@ const ProjectManage: React.FC = () => {
               )}
             />
           </div>
+        </div>
+      </Modal>
+      <Modal
+        open={openDissolve}
+        title={<div style={{ fontWeight: 'bold' }}>Dissolve Project</div>}
+        onOk={onAddConfirmClicked}
+        onCancel={onAddCancelClicked}
+        footer={[
+          <Button key="cancel" onClick={onDissolveCancelClicked}>Cancel</Button>,
+          <Button danger key="confirm" onClick={onDissolveConfirmClicked}>Dissolve</Button>
+        ]}
+        width={1000}
+      >
+        <div className="dissolve-container">
+          <div className="dissolve-warning">Please read carefully. This work is irreversible.</div>
+          <p className="dissolve-desc">This action <strong>cannot</strong> be undone. This will permanently delete the <strong>{projectInfo?.name}</strong> project, documents, schedules and every task infos.</p>
+          <p className="dissolve-desc">This will not affect your status as member of Sprint. If you want to exit this project, you can be deleted by authorzied project members.</p>
+          <p className="dissolve-desc">Please type <strong>{projectInfo?.name}</strong> to confirm.</p>
+          <Input value={dissolve} onChange={(e) => { setDissolve(e.target.value); }} />
         </div>
       </Modal>
     </>
