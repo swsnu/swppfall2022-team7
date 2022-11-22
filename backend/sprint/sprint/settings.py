bd231559ec892b622754ecad97de8ffce9f8d72d
Sanghyun Yi <shyi0211@gmail.com>
2022-11-22 21:02:35 +0900

    fix: change test database to db.sqlite3 (#94)

--- backend/sprint/sprint/settings.py
+++ backend/sprint/sprint/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,6 +99,12 @@ DATABASES = {
         'PORT': os.environ['DATABASE_PORT'],
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'testDatabase'
+    }
 
 
 # Password validation
