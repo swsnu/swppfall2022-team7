60d5b4e4a3e83f53c29c3299389f358b1c033272
Alb3it <98588853+Alb3it@users.noreply.github.com>
2022-11-25 18:18:18 +0900

    feat: project manage test, sign up page test (#98)

---
+++ frontend/sprint/src/pages/SignUp.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./SignUp";
+
+describe('sign up page test', () => {
+    let AD: JSX.Element;
+    beforeAll(() => {
+        AD = <MemoryRouter initialEntries={['/']}><Routes><Route path='/' element={<SignUp />} /><Route path='*' element={<div />} /></Routes></MemoryRouter>;
+    });
+    it('should render without error', () => {
+        render(AD);
+    });
+    it('should render without error', () => {
+        render(AD);
+        const inputs = screen.getAllByRole('textbox');
+        const pass = screen.getByPlaceholderText('Password');
+        const pass2 = screen.getByPlaceholderText('Confirm Password');
+        fireEvent.change(inputs[0], { target: { value: 'name' } });
+        fireEvent.change(inputs[1], { target: { value: 'email' } });
+        fireEvent.change(pass, { target: { value: 'password' } });
+        fireEvent.change(pass2, { target: { value: 'password1' } });
+        fireEvent.change(pass2, { target: { value: 'password' } });
+        const login = screen.getByText('Create Account');
+        fireEvent.click(login);
+    });
+})
