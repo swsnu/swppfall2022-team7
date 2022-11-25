ca83ab0284512d7c385f136bbca344d849b46a16
Sanghyun Yi <shyi0211@gmail.com>
2022-11-25 18:50:09 +0900

    test: make coverage >80% (#99)

--- frontend/sprint/src/pages/ProjectManage.test.tsx
+++ frontend/sprint/src/pages/ProjectManage.test.tsx
@@ -1,8 +1,8 @@
-import { renderWithProviders } from "@utils/mocks";
-import { MemoryRouter, Route, Routes } from "react-router-dom";
-import ProjectManage from "./ProjectManage";
+import { renderWithProviders } from '@utils/mocks';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import ProjectManage from './ProjectManage';
 import { choi, dummyProjects, ProjectType } from '@utils/testDummy';
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, screen } from '@testing-library/react';
 
 const stubInitialState: ProjectType[] = dummyProjects;
 
@@ -76,59 +76,59 @@ const mockState2 = {
 };
 
 describe('project manage page test', () => {
-    let AD: JSX.Element;
-    beforeAll(() => {
-        AD = <MemoryRouter initialEntries={['/1']}><Routes><Route path='/:projectId' element={<ProjectManage />} /><Route path='*' element={<div />} /></Routes></MemoryRouter>;
-        global.matchMedia = global.matchMedia ?? function () {
-            return {
-              addListener: jest.fn(),
-              removeListener: jest.fn()
-            };
-          };
-    });
-    it('should be render without render', () => {
-        renderWithProviders(AD, mockState);
-    });
-    it('should be render without render', () => {
-        renderWithProviders(AD, mockState2);
-    });
-    it('should handle invite', () => {
-        renderWithProviders(AD, mockState2);
-        const Addbutton = screen.getByText('Add a new member');
-        fireEvent.click(Addbutton);
-        const emailInput = screen.getAllByRole('combobox');
-        fireEvent.change(emailInput[0], { target: { value: 'poding84@snu.ac.kr' } });
-        const inviteButton = screen.getByText('Invite');
-        fireEvent.click(inviteButton);
-        fireEvent.change(emailInput[0], { target: { value: '*' } });
-        fireEvent.click(inviteButton);
-        const confirmButton = screen.getByText('Confirm');
-        fireEvent.click(confirmButton);
-    });
-    it('should handle invite cancel', () => {
-        renderWithProviders(AD, mockState2);
-        const Addbutton = screen.getByText('Add a new member');
-        fireEvent.click(Addbutton);
-        const cancelButton = screen.getByText('Cancel');
-        fireEvent.click(cancelButton);
-    });
-    it('should handle dissolve project', () => {
-        renderWithProviders(AD, mockState2);
-        const dissolveButton = screen.getAllByText('Dissolve Project');
-        fireEvent.click(dissolveButton[1]);
-        const warningInput = screen.getAllByRole('textbox');
-        fireEvent.change(warningInput[0], { target: { value: 'SPRINT' } });
-        const dissolveB = screen.getByText('Dissolve');
-        fireEvent.click(dissolveB);
-    });
-    it('should handle dissolve project', () => {
-        renderWithProviders(AD, mockState2);
-        const dissolveButton = screen.getAllByText('Dissolve Project');
-        fireEvent.click(dissolveButton[1]);
-        const cancelButton = screen.getByText('Cancel');
-        fireEvent.click(cancelButton);
-    });
-    it('should handle when no params', () => {
-        renderWithProviders(<MemoryRouter initialEntries={['/']}><Routes><Route path='/' element={<ProjectManage />} /><Route path='*' element={<div />} /></Routes></MemoryRouter>, mockState2);
-    });
+  let AD: JSX.Element;
+  beforeAll(() => {
+    AD = <MemoryRouter initialEntries={['/1']}><Routes><Route path='/:projectId' element={<ProjectManage />} /><Route path='*' element={<div />} /></Routes></MemoryRouter>;
+    global.matchMedia = global.matchMedia ?? function () {
+      return {
+        addListener: jest.fn(),
+        removeListener: jest.fn()
+      };
+    };
+  });
+  it('should be render without render', () => {
+    renderWithProviders(AD, mockState);
+  });
+  it('should be render without render', () => {
+    renderWithProviders(AD, mockState2);
+  });
+  it('should handle invite', () => {
+    renderWithProviders(AD, mockState2);
+    const Addbutton = screen.getByText('Add a new member');
+    fireEvent.click(Addbutton);
+    const emailInput = screen.getAllByRole('combobox');
+    fireEvent.change(emailInput[0], { target: { value: 'poding84@snu.ac.kr' } });
+    const inviteButton = screen.getByText('Invite');
+    fireEvent.click(inviteButton);
+    fireEvent.change(emailInput[0], { target: { value: '*' } });
+    fireEvent.click(inviteButton);
+    const confirmButton = screen.getByText('Confirm');
+    fireEvent.click(confirmButton);
+  });
+  it('should handle invite cancel', () => {
+    renderWithProviders(AD, mockState2);
+    const Addbutton = screen.getByText('Add a new member');
+    fireEvent.click(Addbutton);
+    const cancelButton = screen.getByText('Cancel');
+    fireEvent.click(cancelButton);
+  });
+  it('should handle dissolve project', () => {
+    renderWithProviders(AD, mockState2);
+    const dissolveButton = screen.getAllByText('Dissolve Project');
+    fireEvent.click(dissolveButton[1]);
+    const warningInput = screen.getAllByRole('textbox');
+    fireEvent.change(warningInput[0], { target: { value: 'SPRINT' } });
+    const dissolveB = screen.getByText('Dissolve');
+    fireEvent.click(dissolveB);
+  });
+  it('should handle dissolve project', () => {
+    renderWithProviders(AD, mockState2);
+    const dissolveButton = screen.getAllByText('Dissolve Project');
+    fireEvent.click(dissolveButton[1]);
+    const cancelButton = screen.getByText('Cancel');
+    fireEvent.click(cancelButton);
+  });
+  it('should handle when no params', () => {
+    renderWithProviders(<MemoryRouter initialEntries={['/']}><Routes><Route path='/' element={<ProjectManage />} /><Route path='*' element={<div />} /></Routes></MemoryRouter>, mockState2);
+  });
 });
