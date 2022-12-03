e5ee724a7c300c99eabeda8ef9fea91910c847b9
rosyrosie <93393389+rosyrosie@users.noreply.github.com>
2022-12-03 16:43:15 +0900

    [dev-frontend-login] Implement user logic (#107)
    * feat: link zustand and login
    
    * feat: add api variables and keypress login
    
    * feat: implement signout
    
    * feat: implement signup

--- frontend/sprint/src/pages/SignUp.tsx
+++ frontend/sprint/src/pages/SignUp.tsx
@@ -1,19 +1,30 @@
-import { Button, Input } from 'antd';
+import useBindStore from '@store/zustand';
+import { Button, Input, message } from 'antd';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp: React.FC = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirm, setConfirm] = useState('');
+  const signUp = useBindStore(state => state.signUp);
   const enableSignup: boolean = (
     name !== '' &&
     email !== '' &&
     password === confirm &&
     password !== ''
   );
-  const handleSignup: () => void = () => {
+  const handleSignup: () => Promise<void> = async () => {
+    try {
+      await signUp(name, email, password);
+      await message.success('Signup complete! Please login');
+      navigate('/login');
+    } catch (error) {
+      await message.error('The email already exists!');
+      console.log(error);
+    }
   };
   return (
     <div className="signup-body">
@@ -23,7 +34,7 @@ const SignUp: React.FC = () => {
         <Input className="form-input" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
         <Input.Password className="form-input" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
         <Input.Password className="form-input" status={password !== confirm ? 'error' : ''} placeholder="Confirm Password" value={confirm} onChange={e => setConfirm(e.target.value)} />
-        <Button type="primary" disabled={!enableSignup} onClick={handleSignup}>Create Account</Button>
+        <Button type="primary" disabled={!enableSignup} onClick={() => { void handleSignup(); }}>Create Account</Button>
       </div>
       <div>
         {'Have an account?'}&nbsp;&nbsp;<Link to="/login">Login!</Link>
