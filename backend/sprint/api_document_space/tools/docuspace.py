202c498ee57172e02d92a3b0d82d15ba9f14fda5
Sanghyun Yi <shyi0211@gmail.com>
2022-12-03 19:19:04 +0900

    feat: fill docuspace view funcs

---
+++ backend/sprint/api_document_space/tools/docuspace.py
@@ -0,0 +1,14 @@
+from model_project.models import Task, Project, UserProject, DocumentSpace
+from model_user.models import get_user_model
+
+def get_docuspace(project: Project):
+    pass
+
+def create_docuspace(project: Project):
+    pass
+
+def get_docuspace_detail(docuspace: DocumentSpace):
+    pass
+
+def edit_docuspace_detail(docuspace: DocumentSpace):
+    pass
--- backend/sprint/api_document_space/views.py
+++ backend/sprint/api_document_space/views.py
@@ -1,36 +1,88 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
+from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
+from utility.custom_decorator import (
+    return_bad_request_if_anonymous,
+    return_bad_request_if_exception,
+    return_bad_request_if_does_not_exist
+)
+from utility.serializers import (
+    BaseResponse
+)
+from model_project.models import Task, Project, UserProject, DocumentSpace
+from model_user.models import get_user_model
+from .tools.docuspace import (
+    get_docuspace, create_docuspace, get_docuspace_detail,
+    edit_docuspace_detail
+)
 
 # Create your views here.
+@api_view(['GET'])
 @require_http_methods(['GET'])
+@return_bad_request_if_anonymous
+@return_bad_request_if_does_not_exist
 def docuspace(request, project_id:int):
     '''
     [GET] Get document space list of the project
     '''
-    # TODO
-    return HttpResponse(status=200)
+    project = Project.objects.get(id=project_id)
+    user = request.user
+    if not UserProject.objects.filter(user=user, project=project).exists():
+        return HttpResponse(status=403)
+    ret = get_docuspace(project)
+    return JsonResponse(ret, status=200)
 
+@api_view(['POST'])
 @require_http_methods(['POST'])
+@return_bad_request_if_exception
+@return_bad_request_if_anonymous
+@return_bad_request_if_does_not_exist
 def m_docuspace(request, project_id:int):
     '''
     [POST] Create new document space
     '''
-    # TODO
-    return HttpResponse(status=200)
+    project = Project.objects.get(id=project_id)
+    user = request.user
+    if not UserProject.objects.filter(user=user, project=project).exists():
+        return HttpResponse(status=403)
+    ret = create_docuspace(project)
+    return JsonResponse(ret, status=201)
 
+@api_view(['GET'])
 @require_http_methods(['GET'])
+@return_bad_request_if_anonymous
+@return_bad_request_if_does_not_exist
 def docuspace_detail(request, docuspace_id:int):
     '''
     [GET] Get document space detail
     '''
-    # TODO
-    return HttpResponse(status=200)
+    docuspace = DocumentSpace.objects.get(id=docuspace_id)
+    project = docuspace.project
+    user = request.user
+    if not UserProject.objects.filter(user=user, project=project).exists():
+        return HttpResponse(status=403)
+    ret = get_docuspace_detail(docuspace)
+    return JsonResponse(ret, status=200)
 
+@api_view(['PUT', 'DELETE'])
 @require_http_methods(['PUT', 'DELETE'])
+@return_bad_request_if_exception
+@return_bad_request_if_anonymous
+@return_bad_request_if_does_not_exist
 def m_docuspace_detail(request, docuspace_id:int):
     '''
     [PUT] Change document space detail
     [DELETE] Delete document space
     '''
-    # TODO
-    return HttpResponse(status=200)
+    docuspace = DocumentSpace.objects.get(id=docuspace_id)
+    project = docuspace.project
+    user = request.user
+    if not UserProject.objects.filter(user=user, project=project).exists():
+        return HttpResponse(status=403)
+    if request.method == 'PUT':
+        ret = edit_docuspace_detail(docuspace)
+        return JsonResponse(ret, status=200)
+    elif request.method == 'DELETE':
+        docuspace.delete()
+        return HttpResponse(status=204)
