75635f4cc64efba0427da21807977149e43b9379
Alb3it <hjjoo389@gmail.com>
2022-12-05 02:03:30 +0900

    feat: implement test for Project main a little

--- frontend/sprint/src/layouts/ProjectMain.test.tsx
+++ frontend/sprint/src/layouts/ProjectMain.test.tsx
@@ -1,85 +1,145 @@
-// import { act, fireEvent, screen } from '@testing-library/react';
-// import { MemoryRouter, Route, Routes } from 'react-router';
-// import ProjectMain from './ProjectMain';
-// import { renderWithProviders } from '@utils/mocks';
-// import { dummyProjects, DProjectType } from '@utils/testDummy';
+import useBindStore from "@store/zustand";
+import { ProjectType } from "@store/zustand/project";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { Route, Routes, MemoryRouter } from "react-router-dom";
+import axios from "axios";
+import ProjectMain from "./ProjectMain";
 
-// const mockNavigate = jest.fn();
-// jest.mock('react-router', () => ({
-//   ...jest.requireActual('react-router'),
-//   Navigate: (props: any) => {
-//     mockNavigate(props.to);
-//     return null;
-//   },
-//   useNavigate: () => mockNavigate
-// }));
+const mockNavigate = jest.fn();
+jest.mock('react-router', () => ({ ...jest.requireActual('react-router'), useNavigate: () => mockNavigate }));
 
-// const stubInitialState: DProjectType[] = dummyProjects;
 
-// const mockStore = { preloadedState: { project: stubInitialState } };
+const fakeProject1: ProjectType = {
+  document_number: 1,
+  id: 1,
+  last_modified: 'a',
+  manager: 1,
+  member_list: [
+    {
+      email: 'a',
+      id: 1,
+      username: 'a'
+    }
+  ],
+  name: 'a',
+  subject: 'a',
+  document_space_list: [
+    {
+      created_at: 'a',
+      head: 1,
+      id: 1,
+      name: 'a'
+    },
+    {
+      created_at: 'b',
+      head: 2,
+      id: 2,
+      name: 'b'
+    }
+  ],
+  task_list: [
+    {
+      assignee: {
+        email: 'a',
+        id: 1,
+        username: 'a'
+      },
+      content: 'a',
+      createdAt: 'a',
+      id: 1,
+      name: 'a',
+      project: 1,
+      untilAt: 'a',
+      updatedAt: 'a'
+    }
+  ]
+};
 
-// describe('project main test', () => {
-//   let AD: JSX.Element;
-//   beforeAll(() => {
-//     AD = <MemoryRouter initialEntries={['/1']}><Routes><Route path='/:projectId' element={<ProjectMain />} /></Routes></MemoryRouter>;
-//     global.matchMedia = global.matchMedia ?? function () {
-//       return {
-//         addListener: jest.fn(),
-//         removeListener: jest.fn()
-//       };
-//     };
-//   });
-//   it('should render without render', async () => {
-//     renderWithProviders(<ProjectMain />, mockStore);
-//     const menubuttons = screen.getAllByRole('menuitem');
-//     await act(async () => { fireEvent.click(menubuttons[0]); });
-//   });
-//   it('should handle with menu item clicked', async () => {
-//     renderWithProviders(AD, mockStore);
-//     const buttons = screen.getAllByRole('menuitem');
-//     await act(async () => { fireEvent.click(buttons[0]); });
-//     await act(async () => { fireEvent.click(buttons[1]); });
-//     await act(async () => { fireEvent.click(buttons[2]); });
-//     await act(async () => { fireEvent.click(buttons[3]); });
-//     await act(async () => { fireEvent.click(buttons[4]); });
-//     await act(async () => { fireEvent.click(buttons[5]); });
-//   });
-//   it('should handle when project Id is undefined', async () => {
-//     AD = <MemoryRouter initialEntries={['/6']}><Routes><Route path="/:project" element={<ProjectMain />} /> </Routes></MemoryRouter>;
-//     renderWithProviders(AD, mockStore);
-//     const buttons = screen.getAllByRole('menuitem');
-//     await act(async () => { fireEvent.click(buttons[3]); });
-//   });
-//   it('should handle when key is intro', async () => {
-//     AD = <MemoryRouter initialEntries={['/1/1/1']}><Routes><Route path="/:projectId/:taskId/:menuId" element={<ProjectMain />} /> </Routes></MemoryRouter>;
-//     renderWithProviders(AD, mockStore);
-//     const buttons = screen.getAllByRole('menuitem');
-//     await act(async () => { fireEvent.click(buttons[0]); });
-//     await act(async () => { fireEvent.click(buttons[1]); });
-//     const taskButtons = screen.getAllByRole('menuitem');
-//     await act(async () => { fireEvent.click(taskButtons[2]); });
-//     await act(async () => { fireEvent.click(taskButtons[3]); });
-//     await act(async () => { fireEvent.click(taskButtons[4]); });
-//     await act(async () => { fireEvent.click(taskButtons[5]); });
-//   });
-//   it('should handle when there is no task', async () => {
-//     AD = <MemoryRouter initialEntries={['/1/1']}><Routes><Route path="/:projectId/:menuId" element={<ProjectMain />} /> </Routes></MemoryRouter>;
-//     renderWithProviders(AD, mockStore);
-//     const buttons = screen.getAllByRole('menuitem');
-//     await act(async () => { fireEvent.click(buttons[2]); });
-//   });
-//   it('should handle when there is wrong menu', async () => {
-//     AD = <MemoryRouter initialEntries={['/1/1']}><Routes><Route path="/:projectId/:taskId" element={<ProjectMain />} /> </Routes></MemoryRouter>;
-//     renderWithProviders(AD, mockStore);
-//     const buttons = screen.getAllByRole('menuitem');
-//     await act(async () => { fireEvent.click(buttons[2]); });
-//   });
-// });
+const fakeProject2: ProjectType = {
+  document_number: 2,
+  id: 2,
+  last_modified: 'a',
+  manager: 2,
+  member_list: [
+    {
+      email: 'a',
+      id: 1,
+      username: 'a'
+    }
+  ],
+  name: 'b',
+  subject: 'b',
+  document_space_list: [
+    {
+      created_at: 'a',
+      head: 1,
+      id: 1,
+      name: 'a'
+    },
+    {
+      created_at: 'b',
+      head: 2,
+      id: 2,
+      name: 'b'
+    }
+  ],
+  task_list: [
+    {
+      assignee: {
+        email: 'a',
+        id: 1,
+        username: 'a'
+      },
+      content: 'b',
+      createdAt: 'b',
+      id: 2,
+      name: 'b',
+      project: 2,
+      untilAt: 'b',
+      updatedAt: 'b'
+    }
+  ]
+}
 
-describe('dummy', () => {
-  it('d', () => {
-      const a = 1;
+const fakeProjects: ProjectType[] = [ fakeProject1, fakeProject2 ];
+
+const fakeProjectReturn = {
+  data: fakeProject1
+};
+
+describe('ProjectMain Test', () => {
+  let AD: JSX.Element;
+  beforeAll(() => {
+    global.matchMedia = global.matchMedia ?? function () {
+      return {
+        addListener: jest.fn(),
+        removeListener: jest.fn()
+      };
+    };
+  });
+  it('should render without error', async () => {
+    let i = useBindStore.getState();
+    i.projects = fakeProjects;
+    useBindStore.setState(i, true);
+    axios.get = jest.fn().mockResolvedValue(fakeProjectReturn);
+    AD = <MemoryRouter initialEntries={['/1']}><Routes><Route path='/:projectId' element={<ProjectMain />} /></Routes></MemoryRouter>;
+    render(AD);
+    await waitFor(() => { expect(useBindStore.getState().selectedProject).toEqual(fakeProject1) });
+    const buttons = screen.getAllByRole('menuitem');
+    fireEvent.click(buttons[2]);
+    await waitFor(() => { expect(mockNavigate).toBeCalled() });
+    fireEvent.click(buttons[3]);
+    await waitFor(() => { expect(mockNavigate).toBeCalled() });
+    fireEvent.click(buttons[4]);
+    await waitFor(() => { expect(mockNavigate).toBeCalled() });
+    fireEvent.click(buttons[5]);
+    await waitFor(() => { expect(mockNavigate).toBeCalled() });
+    fireEvent.click(buttons[0]);
+    await waitFor(() => { expect(mockNavigate).toBeCalled() });
+    fireEvent.click(buttons[1]);
+    await waitFor(() => { expect(mockNavigate).toBeCalled() });
+    const nButtons = screen.getAllByRole('menuitem');
+    fireEvent.click(nButtons[2]);
+    await waitFor(() => { expect(mockNavigate).toBeCalled() });
   });
 });
-
-export {};
