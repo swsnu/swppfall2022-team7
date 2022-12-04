d2376697e3b9414a04da6c1020204a052323a885
Alb3it <hjjoo389@gmail.com>
2022-12-04 22:44:26 +0900

    feat: implement test for UserCard Component

---
+++ frontend/sprint/src/components/UserCard.test.tsx
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import UserCard from "./UserCard";
+
+describe('UserCard test', () => {
+  let AD: JSX.Element;
+  beforeAll(() => {
+    AD = <UserCard user={{ email: "e", username: "a" }}/>;
+    global.matchMedia = global.matchMedia ?? function () {
+      return {
+        addListener: jest.fn(),
+        removeListener: jest.fn()
+      };
+    };
+  });
+  it('should render without error', () => {
+    render(AD); 
+  });
+});
