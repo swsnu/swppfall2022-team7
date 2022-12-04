1d666884c31b9b7c13dfc2b32428b6c0b7b2c7c3
rosyrosie <kshunn@snu.ac.kr>
2022-12-04 21:56:56 +0900

    feat: link Document Space with api

--- frontend/sprint/src/store/zustand/documentSpace.ts
+++ frontend/sprint/src/store/zustand/documentSpace.ts
@@ -1,5 +1,7 @@
+import { GET_DOC_SPACES_URL } from './../../services/api';
 import { StateCreator } from 'zustand';
 import { SliceType } from '.';
+import axios from 'axios';
 
 export interface DocumentSpaceType {
   id: number
@@ -24,7 +26,10 @@ SliceType,
 DocumentSpaceSlice
 > = (set, get) => ({
   documentSpaces: [],
-  getDocumentSpaces: async (projectId: number) => {},
+  getDocumentSpaces: async (projectId: number) => {
+    const res = await axios.get(GET_DOC_SPACES_URL(projectId));
+    set({ documentSpaces: res.data });
+  },
   addDocumentSpace: async () => {},
   editDocumentSpace: async () => {},
   changeDocumentSpaceHead: async (documentId: number) => {},
