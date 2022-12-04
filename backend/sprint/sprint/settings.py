5d64f1503f05a2bf26ca54693f6e1d7729668f90
Seokwoo Choi <54700617+poding84@users.noreply.github.com>
2022-12-04 17:38:13 +0900

    add cors whitelist (#128)

--- backend/sprint/sprint/settings.py
+++ backend/sprint/sprint/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://api.swppsprint.site',
+    'http://api.swppsprint.site'
 )
 
 # Application definition
