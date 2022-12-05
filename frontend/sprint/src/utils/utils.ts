693de3472617b5e2e877ac6fc7288e15260b98af
rosyrosie <kshunn@snu.ac.kr>
2022-12-05 18:43:12 +0900

    feat: change crypto random function

--- frontend/sprint/src/utils/utils.ts
+++ frontend/sprint/src/utils/utils.ts
@@ -1,7 +1,10 @@
 export const iconString = (name: string): string => name.substring(0, 1).toUpperCase();
 export const shuffleList = (list: number[]): number[] => {
+  const crypto = window.crypto;
   for (let i = list.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const random = new Uint8Array(1);
+    crypto.getRandomValues(random);
+    const j = Math.floor((random[0] / Math.pow(2, 8)) * (i + 1));
     [list[i], list[j]] = [list[j], list[i]];
   }
   return list;
