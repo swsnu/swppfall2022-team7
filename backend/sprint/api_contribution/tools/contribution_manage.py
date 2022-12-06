bc81bd1d59ec9bed2aa7e225adab99287cdbce76
Seokwoo Choi <a42072130@gmail.com>
2022-12-06 14:33:00 +0900

    feat: fix timeline format

--- backend/sprint/api_contribution/tools/contribution_manage.py
+++ backend/sprint/api_contribution/tools/contribution_manage.py
@@ -1,6 +1,8 @@
 from model_project.models import Task, Project, TaskDocumentSpace, DocumentSpace, UserProject, UserProjectActivity
 from api_user.tools.account import convert_user_to_dict
 from model_user.tools.noti_manage import convert_upa_to_message
+from utility.date_string import date_to_string
+
 
 from django.contrib.auth.models import User
 
@@ -35,11 +37,21 @@ def get_member_quest_board(project_id: int):
 
 def get_project_timeline(project_id: int):
     project = Project.objects.get(id=project_id)
-    qs_upa = UserProjectActivity.objects.filter(user_project__project = project).select_related('user_project__user').order_by('-created_at')
+    qs_upa = UserProjectActivity.objects.filter(user_project__project = project) \
+        .select_related('user_project') \
+        .select_related('user_project__user') \
+        .select_related('user_project__project') \
+        .order_by('-created_at')
     ret = []
     for upa in qs_upa:
         upa: UserProjectActivity
-        ret.append({
+        if len(ret) == 0 or ret[-1]["date"] != upa.created_at.date() :
+            ret.append({
+                "date": upa.created_at.date(),
+                "date_str": date_to_string(upa.created_at.date()),
+                "logs": []
+            })
+        ret[-1]["logs"].append({
             "user": convert_user_to_dict(upa.user_project.user),
             "created_at": upa.created_at,
             "message": convert_upa_to_message(upa)
