5372c1b98824fb89d888010b9966ee784fe12c7e
Seokwoo Choi <a42072130@gmail.com>
2022-12-06 15:15:37 +0900

    fix pylint

--- backend/sprint/utility/date_string.py
+++ backend/sprint/utility/date_string.py
@@ -5,37 +5,42 @@ def date_to_string(date_time: datetime):
     delta_minute = (now-date_time).seconds//60
     delta_day = (now-date_time).days
     if now < date_time + timedelta(minutes=1) :
-        return "few seconds ago"
+        date_str = "few seconds ago"
     elif now < date_time + timedelta(minutes=5) :
-        return "few minutes ago"
+        date_str = "few minutes ago"
     elif now < date_time + timedelta(minutes=60) :
-        return f"{delta_minute} minutes ago"
+        date_str = f"{delta_minute} minutes ago"
     elif now < date_time + timedelta(hours=3) :
-        return "few hours ago"
+        date_str = "few hours ago"
     elif now < date_time + timedelta(hours=24) :
-        return f"{delta_minute//60} hours ago"
+        date_str = f"{delta_minute//60} hours ago"
     elif now < date_time + timedelta(days=2) :
-        return "yesterday"
+        date_str = "yesterday"
     elif now < date_time + timedelta(days=7) :
-        return f"{delta_day} days ago"
+        date_str = f"{delta_day} days ago"
     elif now < date_time + timedelta(days=30) :
-        return f"{delta_day//7} weeks ago"
+        date_str = f"{delta_day//7} weeks ago"
     elif now < date_time + timedelta(days=120) :
-        return "few months ago"
-    
-    return datetime.strftime(date_time, '%Y-%m-%d')
+        date_str = "few months ago"
+    else :
+        date_str = datetime.strftime(date_time, '%Y-%m-%d')
+
+    return date_str
 
 def date_to_string_day(date_time: date):
-    if type(date_time) == datetime :
+    if date_time.isinstance(datetime) :
         date_time = date_time.date()
-        
+
     if datetime.now().date() == date_time :
-        return "Today"
+        date_str = "Today"
     elif datetime.now().date() == timedelta(days=1) + date_time:
-        return "Yesterday"
-    
-    return datetime.strftime(date_time, '%b %-d, %Y')
+        date_str = "Yesterday"
+    else :
+        date_str = datetime.strftime(date_time, '%b %-d, %Y')
+
+    return date_str
 
 def string_to_date(date_string: str):
     ''' It should change string to date '''
-    return datetime.strptime(date_string, '%Y-%m-%d')
+    date_str = datetime.strptime(date_string, '%Y-%m-%d')
+    return date_str
