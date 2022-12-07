b444c47438f5d6e58e3ce8ef6871069a97c85314
Alb3it <hjjoo389@gmail.com>
2022-12-07 16:55:29 +0900

    feat: implement test for Login page

--- frontend/sprint/src/pages/Login.test.tsx
+++ frontend/sprint/src/pages/Login.test.tsx
@@ -1,5 +1,7 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fakeUser1 } from '@utils/testDummy';
 import axios from 'axios';
+import { act } from 'react-dom/test-utils';
 import { MemoryRouter } from 'react-router-dom';
 import Login from './Login';
 
@@ -7,29 +9,51 @@ const mockNavigate = jest.fn();
 jest.mock('react-router', () => ({ ...jest.requireActual('react-router'), useNavigate: () => mockNavigate }));
 
 describe('test Login page', () => {
+  function createMockLocalStorage (storage: any): void {
+    const localStorageMock = (function () {
+      let store: any = storage;
+      return {
+        getItem: function (key: string) {
+          return store[key] ?? null;
+        },
+        setItem: function (key: string, value: string) {
+          store[key] = value.toString();
+        },
+        clear: function () {
+          store = {};
+        }
+      };
+    })();
+    Object.defineProperty(window, 'localStorage', {
+      value: localStorageMock
+    });
+  };
   let AD: JSX.Element;
   beforeAll(() => {
     AD = <MemoryRouter><Login /></MemoryRouter>;
   });
-  it('should handle login fail', async () => {
+  it('should handle login success', async () => {
+    axios.post = jest.fn().mockResolvedValue({ data: { token: "1", ...fakeUser1 } });
+    localStorage.setItem = jest.fn();
+    createMockLocalStorage({ token: "a" });
     render(AD);
     const email = screen.getByPlaceholderText('Example@snu.ac.kr');
     const password = screen.getByPlaceholderText('Password');
     fireEvent.change(email, { target: { value: 'testemail' } });
     fireEvent.change(password, { target: { value: 'testpassword' } });
     fireEvent.keyPress(password, { key: 'p', code: 112, charCode: 112 });
-    fireEvent.keyPress(password, { key: 'Enter', code: 13, charCode: 13 });
-    await waitFor(() => { expect(screen.getByText('Wrong email or password')).toBeInTheDocument(); });
+    await act(async () => { fireEvent.click(screen.getByRole('button')); });
+    await waitFor(() => { expect(mockNavigate).toBeCalled(); });
   });
-  it('should handle handle success', async () => {
-    axios.post = jest.fn().mockResolvedValueOnce({ data: { token: 'fakeToken' } });
+  it('should handle login failed', async () => {
+    axios.post = jest.fn().mockResolvedValue({ data: { token: null, user: fakeUser1 } });
+    createMockLocalStorage({ token: null });
     render(AD);
     const email = screen.getByPlaceholderText('Example@snu.ac.kr');
     const password = screen.getByPlaceholderText('Password');
     fireEvent.change(email, { target: { value: 'testemail' } });
     fireEvent.change(password, { target: { value: 'testpassword' } });
-    const login = screen.getByRole('button');
-    fireEvent.click(login);
-    await waitFor(() => { expect(mockNavigate).toBeCalled(); });
+    fireEvent.keyPress(password, { key: 'p', code: 112, charCode: 112 });
+    await act(async () => { fireEvent.keyPress(password, { key: 'Enter', code: 13, charCode: 13 }); });
   });
 });
