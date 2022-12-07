917fc7e1a8ba97b042ee1c3887875a430e589378
Alb3it <hjjoo389@gmail.com>
2022-12-07 16:37:27 +0900

    feat: implement test for signup page

--- frontend/sprint/src/pages/SignUp.test.tsx
+++ frontend/sprint/src/pages/SignUp.test.tsx
@@ -1,8 +1,15 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import axios from 'axios';
+import { time } from 'console';
+import { act } from 'react-dom/test-utils';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './SignUp';
 
-describe('sign up page test', () => {
+const mockNavigate = jest.fn();
+jest.mock('react-router', () => ({ ...jest.requireActual('react-router'), useNavigate: () => mockNavigate }));
+jest.useFakeTimers();
+
+describe('<SignUp />', () => {
   let AD: JSX.Element;
   beforeAll(() => {
     AD = <MemoryRouter initialEntries={['/']}><Routes><Route path='/' element={<SignUp />} /><Route path='*' element={<div />} /></Routes></MemoryRouter>;
@@ -10,7 +17,8 @@ describe('sign up page test', () => {
   it('should render without error', () => {
     render(AD);
   });
-  it('should render without error', async () => {
+  it('should handle sign up success', async () => {
+    axios.post = jest.fn();
     render(AD);
     const inputs = screen.getAllByRole('textbox');
     const pass = screen.getByPlaceholderText('Password');
@@ -20,8 +28,24 @@ describe('sign up page test', () => {
     fireEvent.change(pass, { target: { value: 'password' } });
     fireEvent.change(pass2, { target: { value: 'password1' } });
     fireEvent.change(pass2, { target: { value: 'password' } });
-    const login = screen.getByText('Create Account');
-    fireEvent.click(login);
-    await waitFor(() => { expect(screen.getByText('The email already exists!')).toBeInTheDocument(); });
+    const signUp = screen.getByText('Create Account');
+    await act(async () => { fireEvent.click(signUp); });
+    await act(async () => { jest.advanceTimersByTime(4000); });
+  });
+  it('should handle sign up error', async () => {
+    jest.spyOn(axios, 'post').mockRejectedValue(new Error('error'));
+    jest.spyOn(global.console, 'log').mockImplementation(() => {});
+    render(AD);
+    const inputs = screen.getAllByRole('textbox');
+    const pass = screen.getByPlaceholderText('Password');
+    const pass2 = screen.getByPlaceholderText('Confirm Password');
+    fireEvent.change(inputs[0], { target: { value: 'name' } });
+    fireEvent.change(inputs[1], { target: { value: 'email' } });
+    fireEvent.change(pass, { target: { value: 'password' } });
+    fireEvent.change(pass2, { target: { value: 'password1' } });
+    fireEvent.change(pass2, { target: { value: 'password' } });
+    const signUp = screen.getByText('Create Account');
+    await act(async () => { fireEvent.click(signUp); });
+    await act(async () => { jest.advanceTimersByTime(4000); });
   });
 });
