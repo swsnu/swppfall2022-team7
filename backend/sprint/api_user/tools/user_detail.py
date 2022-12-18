c45b70980a295ea93870c6e8711ae2da3aa669e2
Sanghyun Yi <shyi0211@gmail.com>
2022-12-18 17:28:18 +0900

    test: to get more coverage

--- backend/sprint/api_user/tools/user_detail.py
+++ backend/sprint/api_user/tools/user_detail.py
@@ -5,27 +5,6 @@ from model_user.models import Timetable, Image
 from django.contrib.auth.models import User
 from django.conf import settings
 
-
-def get_timetable_of_user(user: User):
-    tt = None
-    if Timetable.objects.filter(user=user).exists():
-        tt = Timetable.objects.get(user=user)
-    if tt is None:
-        tt = Timetable.objects.create(user=user, schedule=create_initial_timetable())
-    return tt.schedule
-        
-def create_initial_timetable():
-    ret = []
-    week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    ti = datetime.datetime(year=2022, month=1, day=1, hour=9)
-    while ti < datetime.datetime(year=2022, month=1, day=1, hour=23, minute=59):
-        ret.append({
-            'time': datetime.datetime.strftime(ti, '%H:%M'),
-            'board': { w:'Freetime' for w in week }
-        })
-        ti += datetime.timedelta(minutes=30)
-    return ret
-
 def upload_profile(user: User, img): 
     image = Image.objects.filter(user=user)
     if image.exists() :
