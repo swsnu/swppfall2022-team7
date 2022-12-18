d0f298fd0a645dbbd0f4ad000470f509d2a5c58f
Alb3it <hjjoo389@gmail.com>
2022-12-18 14:00:21 +0900

    asdf

--- frontend/sprint/src/pages/ProjectDocument.test.tsx
+++ frontend/sprint/src/pages/ProjectDocument.test.tsx
@@ -1,7 +1,5 @@
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import ProjectDocument from './ProjectDocument';
-import * as AWSMock from 'aws-sdk-mock';
-import AWS from 'aws-sdk';
 import { render, waitFor } from '@testing-library/react';
 import axios from 'axios';
 import { fakeDocumentSpace1 } from '@utils/testDummy';
@@ -11,13 +9,6 @@ describe('<ProjectDocument />', () => {
   let AD: JSX.Element;
   beforeAll(() => {
     jest.useFakeTimers();
-    AWSMock.setSDKInstance(AWS);
-    AWSMock.mock('S3', 'getSignedUrl', (func: string, obj: any) => {
-      return null;
-    });
-    AWSMock.mock('S3', 'listObjects', (params: any, callback: Function) => {
-      callback(null, { Contents: [{ Key: 'file1.csv' }] });
-    });
     AD = <MemoryRouter initialEntries={['/1']}><Routes><Route path='/:projectId' element={<ProjectDocument />} /></Routes></MemoryRouter>;
     global.matchMedia = global.matchMedia ?? function () {
       return {
